3ae1a3ea705bb3d65c7aa96f4cf3122405e4b2f7
Will Harris <will.t.harris@protonmail.com>
2019-09-27 14:21:09 -0600

    add unary examples to Main method

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 
-class Booleans
+class Unary
 {
     public static void Main()
     {
-        bool content = true;
-        bool noContent = false;
+        int unary = 0;
+        int preIncrement;
+        int preDecrement;
+        int postIncrement;
+        int postDecrement;
+        int positive;
+        int negative;
+        sbyte bitNot;
+        bool logNot;
 
-        Console.WriteLine("It is {0} that C# Station provides C# programming language content.", content);
-        Console.WriteLine("The statement above is not {0}.", noContent);
+        preIncrement = ++unary;
+        Console.WriteLine("pre-Increment: {0}", preIncrement); // writes 1 to console
+
+        preDecrement = --unary;
+        Console.WriteLine("pre-Decrement: {0}", preDecrement); // writes 0 to console
+
+        postDecrement = unary--;
+        Console.WriteLine("Post-Decrement: {0}", postDecrement); // writes 0 to console
+
+        postIncrement = unary++;
+        Console.WriteLine("Post-Increment: {0}", postIncrement); // writes -1 to console
+
+        Console.WriteLine("Final value of Unary: {0}", unary); // writes 0 to console
     }
 }
