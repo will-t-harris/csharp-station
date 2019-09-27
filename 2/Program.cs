965ea2e11962dbf9e878094f47796d2f80abe0df
Will Harris <will.t.harris@protonmail.com>
2019-09-27 14:50:11 -0600

    add examples of binary float division, modulus operator, increment shorthand

--- 2/Program.cs
+++ 2/Program.cs
@@ -21,6 +21,14 @@ class Binary
 
         result = x / y;
         Console.WriteLine("x/y: {0}", result); // writes 1 to console (doesn't include remainder because we're doing int division, need float or decimal for remainders)
-        
+
+        floatresult = (float)x / (float)y;
+        Console.WriteLine("x/y(floats): {0}", floatresult); // writes 1.4 to console, because we cast the values to floats here
+
+        result = x % y;
+        Console.WriteLine("x%y: {0}", result); // writes 2 to console, modulus operator, returns the remainder
+
+        result += x;
+        Console.WriteLine("result+=x: {0}", result); // writes 9 to console, 2 from previous operation plus 7 from defined value of x
     }
 }
