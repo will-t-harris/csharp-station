efbf61ce6e0e4aedc97a2f75af1d0b0ba0cadad2
Will Harris <will.t.harris@protonmail.com>
2019-09-27 16:32:42 -0600

    add example of multiple case conditional (if/else if/else)

--- 3/Program.cs
+++ 3/Program.cs
@@ -30,5 +30,27 @@ class IfSelect
         {
             Console.WriteLine("Your number {0} is equal to zero.", myInt);
         }
+
+        // Multiple case decision
+        if (myInt < 0 || myInt == 0)
+        {
+            Console.WriteLine("Your number {0} is less than or equal to zero.", myInt);
+        }
+        else if (myInt > 0 && myInt <= 10)
+        {
+            Console.WriteLine("Your number {0} is in the range from 1 to 10.", myInt);
+        }
+        else if (myInt > 10 && myInt <= 20)
+        {
+            Console.WriteLine("Your number {0} is in the range from 11 to 20.", myInt);
+        }
+        else if (myInt > 20 && myInt <= 30)
+        {
+            Console.WriteLine("Your number {0} is in the range from 21 to 30.", myInt);
+        }
+        else
+        {
+            Console.WriteLine("Your number {0} is greater than 30", myInt);
+        }
     }
 }
