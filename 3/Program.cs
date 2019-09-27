27f4d8759f3a525db965239a3f93131e4e26db11
Will Harris <will.t.harris@protonmail.com>
2019-09-27 16:25:53 -0600

    add example of conditional with braces

---
+++ 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class IfSelect
+{
+    public static void Main()
+    {
+        string myInput;
+        int myInt;
+
+        Console.Write("Please enter a number: ");
+        myInput = Console.ReadLine();
+        myInt = Int32.Parse(myInput);
+
+        // Single decision and action with braces
+        if (myInt > 0)
+        {
+            Console.WriteLine("Your number {0} is greater than zero.", myInt);
+        }
+    }
+}
