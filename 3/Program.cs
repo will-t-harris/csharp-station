8c5a0b46746c31ae6a57194dc021ea395409500e
Will Harris <will.t.harris@protonmail.com>
2019-09-27 16:29:01 -0600

    add example of either/or conditional

--- 3/Program.cs
+++ 3/Program.cs
@@ -20,5 +20,15 @@ class IfSelect
         // Single decision and action without brackets
         if (myInt < 0)
             Console.WriteLine("Your number {0} is less than zero.", myInt);
+
+        // Either/or decision
+        if (myInt != 0)
+        {
+            Console.WriteLine("Your number {0} is not equal to zero.", myInt);
+        }
+        else
+        {
+            Console.WriteLine("Your number {0} is equal to zero.", myInt);
+        }
     }
 }
