6bef3b2b4231ef27f952a2b1625e037ed2e64b6f
Will Harris <will.t.harris@protonmail.com>
2019-09-27 14:32:02 -0600

    second half of examples using unary operators

--- 2/Program.cs
+++ 2/Program.cs
@@ -27,5 +27,19 @@ class Unary
         Console.WriteLine("Post-Increment: {0}", postIncrement); // writes -1 to console
 
         Console.WriteLine("Final value of Unary: {0}", unary); // writes 0 to console
+
+        positive = -postIncrement;
+        Console.WriteLine("Positive: {0}", positive); // writes 1 to console
+
+        negative = +postIncrement;
+        Console.WriteLine("Negative: {0}", negative); // writes -1 to console
+
+        bitNot = 0;
+        bitNot = (sbyte)(~bitNot);
+        Console.WriteLine("Bitwise Not: {0}", bitNot); // writes -1 to console
+
+        logNot = false;
+        logNot = !logNot;
+        Console.WriteLine("Logical Not: {0}", logNot); // writes True to console
     }
 }
